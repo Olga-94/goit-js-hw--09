09be00ad80857678f6a3feaa97eb93c9a6951fc8
Olga-94 <olgabezdetko1994@gmail.com>
2022-05-14 10:56:30 +0300

    правки 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,6 +10,24 @@ refs.stop.setAttribute('disabled', '');
 refs.start.addEventListener("click", startBtnClick);
 refs.stop.addEventListener("click", stopBtnClick);
 
+function startBtnClick() {
+    refs.start.setAttribute('disabled', '');
+    refs.stop.removeAttribute('disabled');
+    timerChange = setInterval(() => {
+        changeRandomBgr(getRandomHexColor());
+    }, 1000);
+
+    console.log('START');
+}
+
+function stopBtnClick() {
+    refs.start.removeAttribute('disabled');
+    refs.stop.setAttribute('disabled', '');
+    clearInterval(timerChange);
+    
+    console.log('STOP');
+}
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
@@ -17,18 +35,3 @@ function getRandomHexColor() {
 function changeRandomBgr(callback) {
     document.body.style.backgroundColor = `${callback}`;
 }
-
-
-function startBtnClick() {
-    refs.start.setAttribute('disabled', '');
-    refs.stop.removeAttribute('disabled');
-    timerChange = setInterval(changeRandomBgr(getRandomHexColor), 1000)
-    console.log('START');
-};
-
-function stopBtnClick() {
-    refs.start.setAttribute('disabled');
-    refs.stop.removeAttribute('disabled', '');
-    clearInterval(timerChange);
-    console.log('STOP');
-};
